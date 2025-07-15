90f926a300b2d73d34bd73bc7d48a7a31b73876f
ratul0407 <rajaulislamratul12@gmail.com>
2025-07-16 01:05:33 +0600

    created plant interface

--- src/modules/plant/plant.interface.ts
+++ src/modules/plant/plant.interface.ts
@@ -1,8 +1,41 @@
-interface IPlant {
+import { Types } from "mongoose";
+
+enum Category {
+  EASY_CARE = "EASY_CARE",
+  HANGING = "HANGING",
+  AIR_PURIFYING = "AIR_PURIFYING",
+  SUN_LOVING = "SUN_LOVING",
+  LOW_LIGHT = "LOW_LIGHT",
+  FLOWERING = "FLOWERING",
+}
+interface IPlantVariant {
+  variantName: string;
+  price: number;
+  inStock: boolean;
+  stock: boolean;
+  image: string[];
+  sku: string;
+}
+interface IStats {
+  rating?: number;
+  wishlist?: number;
+  cart?: number;
+  viewed?: number;
+}
+export interface IPlant {
   name: string;
   description: string;
-  price: string;
-  quantity: number;
-  sold: number;
-  wishlist: number;
+  category: Category;
+  basePrice: number;
+  inStock: boolean;
+  stock: number;
+  mainImage: string[];
+  variant: [IPlantVariant];
+  additionalImages: string[];
+  tags?: string;
+  careInstructions?: string;
+  reviews?: Types.ObjectId[];
+  stats: IStats;
+  question?: Types.ObjectId[];
+  discount?: number;
 }
