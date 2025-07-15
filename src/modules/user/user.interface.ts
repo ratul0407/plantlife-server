c8fd7918a994d63b932269543d52a6bf3fa20f73
ratul0407 <rajaulislamratul12@gmail.com>
2025-07-16 00:56:07 +0600

    created user interface

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -11,17 +11,32 @@ export interface IAuthProvider {
   provider: "google" | "credentials";
   providerId: string;
 }
+interface ICart {
+  plant: Types.ObjectId;
+  quantity: number;
+}
+interface IOrder {
+  all?: Types.ObjectId[];
+  delivered?: Types.ObjectId[];
+  pending?: Types.ObjectId[];
+  cancelled?: Types.ObjectId[];
+}
 export interface IUser {
   name: string;
   email: string;
+  phone?: string;
   picture?: string;
   password?: string;
   isVerified?: string;
   isDeleted?: string;
   isBlocked?: string;
   wishlist?: Types.ObjectId[];
-  orders?: Types.ObjectId[];
+  orders?: IOrder;
   coins?: number;
+  cart?: ICart[];
   role: Role;
   auths: IAuthProvider[];
+  reviews?: Types.ObjectId[];
+  recentlyViewed?: Types.ObjectId[];
+  questions?: Types.ObjectId[];
 }
