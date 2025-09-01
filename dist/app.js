502b5749178f85661cfbd16d987889fb98287ab4
ratul0407 <rajaulislamratul12@gmail.com>
2025-09-01 22:54:41 +0600

    updatead cookie settings

--- dist/app.js
+++ dist/app.js
@@ -25,7 +25,7 @@ app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: [env_1.envVars.FRONTEND_URL, "http://localhost:5173"],
+    origin: env_1.envVars.FRONTEND_URL,
     credentials: true,
 }));
 app.use("/api/v1", routes_1.default);
