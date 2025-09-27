b8c3d177167990967ad0b1cbc3f44cc5f6e0c923
ratul0407 <rajaulislamratul12@gmail.com>
2025-09-27 13:45:21 +0600

    fixed some bugs

--- src/app/modules/plant/plant.route.ts
+++ src/app/modules/plant/plant.route.ts
@@ -7,6 +7,7 @@ import { checkAuth } from "../../middlewares/checkAuth";
 
 const router = Router();
 
+router.get("/all-plants", plantController.getAllPlants);
 router.post(
   "/add-plants",
   checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
@@ -14,6 +15,5 @@ router.post(
   plantController.createPlant
 );
 router.post("/get-local-cart-plants", plantController.getLocalCartPlants);
-router.get("/all-plants", plantController.getAllPlants);
 router.get("/:id", plantController.getSinglePlant);
 export const PlantRoutes = router;
